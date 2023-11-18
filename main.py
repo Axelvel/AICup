ad6fc81d028928f5c7cc676bd83911f88f210028
Axel Velez <axelvel@live.fr>
2023-11-18 17:28:20 +0800

    :sparkles: Added create_labels_dict function

--- main.py
+++ main.py
@@ -1,25 +1,4 @@
-from os import listdir
+import torch
 
-# --------------------------------- CONSTANTS -------------------------------- #
-TRAINING_PATH = "./AICup/dataset/First_Phase_Release(Correction)/First_Phase_Text_Dataset/"
-
-
-# ------------------------------ RETRIEVING DATA ----------------------------- #
-def retrieveData(path):
-    list_file = listdir(path)
-    dataset = ["[CLS]"]
-
-    for file in list_file:
-        with open(path + file) as f:
-            lines = f.readlines()
-        for line in lines:
-            if line != "\n":
-                splitted = line.split()
-                for word in splitted:
-                    dataset.append(word)
-        dataset.append("[SEP]")
-    
-    return dataset
-
-
-
+if __name__ == '__main__':
+    print(torch.__version__)
