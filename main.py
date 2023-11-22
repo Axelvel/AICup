d805d07594265e124f74351f5b458a8e11581545
Axel Velez <axelvel@live.fr>
2023-11-22 20:59:02 +0800

    :sparkles: Added get_labels_types function

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import torch
+from data import retrieveData, create_labels_dict
+
+# --------------------------------- CONSTANTS -------------------------------- #
+
+FIRST_DATASET_PATH = 'dataset/First_Phase_Release(Correction)/First_Phase_Text_Dataset/'
+LABELS_PATH = 'dataset/First_Phase_Release(Correction)/answer.txt'
 
 if __name__ == '__main__':
-    print(torch.__version__)
+    print('Torch version: ', torch.__version__)
+    train_data = retrieveData(FIRST_DATASET_PATH)
+    train_labels_dict = create_labels_dict(LABELS_PATH)
