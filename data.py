1687d716c4b204a4d58771809246657eb252f4a6
Clamorae <trombiniquentin@gmail.com>
2023-11-24 17:02:07 +0800

    create a dataloader

--- data.py
+++ data.py
@@ -1,4 +1,7 @@
+
+import torch
 from os import listdir
+from torch.utils.data import Dataset, DataLoader
 from transformers import BertTokenizer, BertConfig, BertForTokenClassification
 
 # --------------------------------- CONSTANTS -------------------------------- #
@@ -6,6 +9,7 @@ from transformers import BertTokenizer, BertConfig, BertForTokenClassification
 FIRST_DATASET_PATH = 'dataset/First_Phase_Release(Correction)/First_Phase_Text_Dataset/'
 LABELS_PATH = 'dataset/First_Phase_Release(Correction)/answer.txt'
 
+BATCH_SIZE = 62
 
 # ------------------------------ RETRIEVING DATA ----------------------------- #
 def retrieveData(path, dict):
@@ -25,7 +29,6 @@ def retrieveData(path, dict):
                     items.append(word)
 
                     check = False
-                    test = len(labels)
                     for list in raw_data:
                         if word in list and word != list[0] and check == False:
                             labels.append(list[0])
@@ -65,8 +68,11 @@ labels_type = ["OTHER"] + labels_type
 labels_num = len(labels_type)
 
 data,labels = retrieveData(FIRST_DATASET_PATH,train_labels_dict)
-print(len(data))
-print(len(labels))
+label2id = {k: v for v, k in enumerate(list(set(labels)))}
+id2label = {v: k for v, k in enumerate(list(set(labels)))}
+
+print(label2id)
+
 
 
 # ------------------------------ DATA PROCESSING ----------------------------- #
@@ -103,3 +109,36 @@ def tokenize_and_preserve_labels(sentence, labels, tokenizer):
     return tokenized_sentence, tokenized_labels
 
 tk_data,tk_labels = tokenize_and_preserve_labels(data, labels, BertTokenizer.from_pretrained('bert-base-uncased'))
+
+# ------------------------------ DATASET OBJECT ------------------------------ #
+class dataset(Dataset):
+    def __init__(self,data,target):
+        self.data = data
+        self.target = target
+        self.len = len(data)
+
+    def __getitem__(self, index):
+        return {
+            "ids": self.data,
+            "targets": self.target
+        }
+    
+    def __len__(self):
+        return self.len
+    
+
+# ------------------------- TRANSFORM DATA TO TENSOR ------------------------- #
+
+id_data = BertTokenizer.from_pretrained('bert-base-uncased').convert_tokens_to_ids(tk_data)
+id_labels = [label2id[label] for label in tk_labels]
+
+tensor_data = torch.tensor(id_data,dtype=torch.long)
+tensor_labels = torch.tensor(id_labels,dtype=torch.long)
+
+params = {
+    "batch_size": BATCH_SIZE,
+    "shuffle": True,
+    "num_workers": 0
+}
+
+loader = DataLoader(dataset(tensor_data,tensor_labels),**params)
