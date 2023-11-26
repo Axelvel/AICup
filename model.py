e8717ee649625a7d4aa192dd5654ed38fc685d28
Axel Velez <axelvel@live.fr>
2023-11-26 14:41:25 +0800

    :sparkles: Adding training loop

--- model.py
+++ model.py
@@ -1,26 +1,26 @@
 import torch
 import torch.nn as nn
 from transformers import AutoTokenizer, AutoModel
-from data import get_labels_types
-
-LABELS_PATH = 'dataset/First_Phase_Release(Correction)/answer.txt'
-label_types = get_labels_types(LABELS_PATH)
-num_labels = len(label_types)
 
 
 model_name = "bert-base-cased"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 
-class MyModel(nn.Module):
+class PrivacyModel(nn.Module):
 
-    def __init__(self) -> None:
-        super(MyModel, self).__init__()
+    def __init__(self, output_size) -> None:
+        super(PrivacyModel, self).__init__()
+        
+        self.bert_output_size = 768
+        self.output_size = output_size
+
         self.bert = AutoModel.from_pretrained(model_name)
+        self.dropout = nn.Dropout(p=0.2)
+        self.fc = nn.Linear(self.bert_output_size, self.output_size)
 
     def forward(self, x):
         self.bert(x)
+        self.dropout(x)
+        self.fc(x)
         return x
-
-
-model = MyModel()
