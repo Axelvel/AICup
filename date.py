091a3fae0809ee0b0bf24a4a3d795638bac7d5a6
Clamorae <trombiniquentin@gmail.com>
2023-12-08 14:37:08 +0800

    refactor code to make more readable

--- date.py
+++ date.py
@@ -2,16 +2,48 @@ from os import listdir
 
 FIRST_DATASET_PATH = 'dataset/First_Phase_Release(Correction)/First_Phase_Text_Dataset/'
 
-#TODO - add month detection
+def remove_side_char(item):
+    while item[0].isdigit() == False:
+        item = item[1:]
+    while item[-1].isdigit() == False:
+        item = item[:-1]
+    return(item)
+
+def is_possible_date(current_item,digit_number):
+    count = 0
+    possible = ""
+
+    for char in current_item:
+        if char == "/" or char == ".":
+            possible+= "-"
+            count+=1
+        elif char.isdigit():
+            possible+=char
+
+    if count==2:
+        return(possible)
+
+    elif digit_number == 4 or count >2:
+        possible = remove_side_char(current_item)
+        
+        if count >2:
+            return(possible)
+        if len(possible)==4 and int(possible)<2100 and int(possible)>1900 :
+            return("00-00-"+possible)
+    
+    return False
 
 def collect_date(path):
     list_file = listdir(path)
     file_date = []
     for file in list_file:
+
         date=[]
         with open(path + file) as f:
             lines = f.readlines()
+
         for line in lines:
+
             if line != "\n" and sum(char.isdigit() for char in line)>=4:
                 splitted = line.split()
                 for i in range(len(splitted)):
@@ -19,32 +51,11 @@ def collect_date(path):
                     if splitted[i].isdigit():
                         if len(splitted[i])==4 and int(splitted[i])<2100 and int(splitted[i])>1900:
                             date.append("00-00-"+splitted[i])
+
                     if ("/" in splitted[i] or "." in splitted[i]) and digit_number > 3 and digit_number<9:
-                        count = 0
-                        possible = ""
-                        for j,char in enumerate(splitted[i]):
-                            if char == "/" or char == ".":
-                                possible+= "-"
-                                count+=1
-                            elif char.isdigit():
-                                possible+=char
-                        if count==2:
-                            date.append(possible)
-                        elif count>2:
-                            while possible[0] == "-":
-                                possible = possible[1:]
-                            while possible[-1] == "-":
-                                possible = possible[:-1]
-                            date.append(possible)
-                        elif digit_number == 4:
-                            possible = splitted[i]
-                            while possible[0].isdigit() == False:
-                                possible = possible[1:]
-                            while possible[-1].isdigit() == False:
-                                possible = possible[:-1]
-                            if len(possible)==4 and int(possible)<2100 and int(possible)>1900:
-                                date.append("00-00-"+possible)      
-                            
+                        is_possible = is_possible_date(splitted[i],digit_number)
+                        if is_possible != False:
+                            date.append(is_possible)
 
         if date != []:
             file_date.append(date)
@@ -57,6 +68,7 @@ def trivial_date(date):
         file_trivial = []
         max_first = 0
         max_second = 0
+
         for item in file:
             splitted = item.split("-")
             if len(splitted)== 3 and splitted[0]!="" and splitted[1]!="" and splitted[2]!="":
